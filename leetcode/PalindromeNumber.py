112069a888909b65a09b74fde73c4681f55b7460
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-03-30 12:18:32 +0530

    Create PalindromeNumber.py

---
+++ leetcode/PalindromeNumber.py
@@ -0,0 +1,16 @@
+# leetcode - https://leetcode.com/problems/palindrome-number/
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        ranger=1
+        while x//ranger>=10:
+            ranger*=10
+        while x:
+            left=x//ranger
+            right=x%10
+            if left!=right:
+                return False
+            x=(x%ranger)//10
+            ranger//=100
+        return True
