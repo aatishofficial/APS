3efb8ce931933771049f1eb5efb3d404d26175a9
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-03-30 12:27:56 +0530

    Create containerWithMostWater.py

---
+++ leetcode/containerWithMostWater.py
@@ -0,0 +1,12 @@
+# leetcode - https://leetcode.com/problems/container-with-most-water/
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        water=0
+        i,j=0,len(height)-1
+        while i < j:
+            water=max(water,(j-i)*min(height[i],height[j]))
+            if height[i]<height[j]:
+                i+=1
+            else:
+                j-=1
+        return water
