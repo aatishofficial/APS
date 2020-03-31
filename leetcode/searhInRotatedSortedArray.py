cd5fa96afc2bb5518275af4655d58319589c0443
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-03-31 21:24:07 +0530

    Create searhInRotatedSortedArray.py

---
+++ leetcode/searhInRotatedSortedArray.py
@@ -0,0 +1,30 @@
+# leetcode - https://leetcode.com/problems/search-in-rotated-sorted-array/
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums)==0:
+            return -1
+        left=0
+        right=len(nums)-1
+        while left<right:
+            mid=left+(right-left)//2
+            if nums[mid] > nums[right]:
+                left=mid+1
+            else:
+                right=mid
+        start=left
+        left=0
+        right=len(nums)-1
+        if target>=nums[start] and target<=nums[right]:
+            left=start
+        else:
+            right=start
+        while left <= right:
+            mid=left+(right-left)//2
+            print(mid)
+            if nums[mid]==target:
+                return mid
+            elif nums[mid] > target:
+                right=mid-1
+            else:
+                left=mid+1
+        return -1
