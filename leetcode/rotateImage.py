688e0512c6c6c6f7d0999f66f81a3718c7dc243d
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-04-01 18:47:04 +0530

    Create rotateImage.py

---
+++ leetcode/rotateImage.py
@@ -0,0 +1,19 @@
+# leetcode - https://leetcode.com/problems/rotate-image/
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n=len(matrix)
+        # transpose
+        for i in range(n):
+            for j in range(i,n):
+                temp=matrix[i][j]
+                matrix[i][j]=matrix[j][i]
+                matrix[j][i]=temp
+        # two pointer operation
+        for i in range(n):
+            for j in range(n//2):
+                temp=matrix[i][j]
+                matrix[i][j]=matrix[i][n-1-j]
+                matrix[i][n-1-j]=temp
