3944876031767f552be56bef7380b77685d37ad3
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-04-02 10:22:13 +0530

    Create jumpGame.py

---
+++ leetcode/jumpGame.py
@@ -0,0 +1,8 @@
+# leetcode - https://leetcode.com/problems/jump-game/
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        lastpos=len(nums)-1
+        for i in range(len(nums)-1,-1,-1):
+            if i+nums[i]>=lastpos:
+                lastpos=i
+        return lastpos==0
