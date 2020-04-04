192526a6502c307bbd60a1b18121ed4a8ea4fee7
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-04-04 18:09:21 +0530

    Create subsets2.py

---
+++ leetcode/subsets2.py
@@ -0,0 +1,13 @@
+# leetcode - https://leetcode.com/problems/subsets-ii/
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res=[]
+        self.dfs(nums,0,[],res)
+        return res
+    def dfs(self,nums,index,path,res):
+        res.append(path)
+        for i in range(index,len(nums)):
+            if i>index and nums[i]==nums[i-1]:
+                continue
+            self.dfs(nums,i+1,path+[nums[i]],res)
