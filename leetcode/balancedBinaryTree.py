bf30cc9825f74f90b3e07381bc88da777f66b950
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-04-07 10:07:51 +0530

    Create balancedBinaryTree.py

---
+++ leetcode/balancedBinaryTree.py
@@ -0,0 +1,17 @@
+# leetcode - https://leetcode.com/problems/balanced-binary-tree/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        return self.isBalanced(root.left) and self.isBalanced(root.right) and abs(self.height(root.left)-self.height(root.right))<=1
+    def height(self,root):
+        if root==None:
+            return 0
+        return max(self.height(root.left),self.height(root.right))+1
