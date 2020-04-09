0636081e5652457e7e4b5dd1dc7f226eddf3d9b7
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-04-09 11:24:04 +0530

    Create single-number-ii.py

---
+++ leetcode/single-number-ii.py
@@ -0,0 +1,9 @@
+# leetcode - https://leetcode.com/problems/single-number-ii
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash_table=defaultdict(int)
+        for i in nums:
+            hash_table[i]+=1
+        for i in hash_table:
+            if hash_table[i]==1:
+                return i
