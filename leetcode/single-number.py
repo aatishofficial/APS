a58d232156b7c6b5bb4c434c9349cb3eff5c3d1a
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-04-09 10:36:00 +0530

    Create single-number.py

---
+++ leetcode/single-number.py
@@ -0,0 +1,7 @@
+# leetcode - https://leetcode.com/problems/single-number
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        a=0
+        for i in nums:
+            a^=i
+        return a
