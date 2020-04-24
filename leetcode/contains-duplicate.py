2dc57169cdc9f4ce380c224aad2259d284ec9374
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-04-24 13:29:39 +0530

    Create contains-duplicate.py

---
+++ leetcode/contains-duplicate.py
@@ -0,0 +1,9 @@
+# leetcode - https://leetcode.com/problems/contains-duplicate/
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        h=set()
+        for num in nums:
+            if num in h:
+                return True
+            h.add(num)
+        return False
