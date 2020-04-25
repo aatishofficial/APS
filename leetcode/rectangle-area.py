067d3bd0ead97e6a9682f990b92472b1f305cc35
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-04-25 10:30:35 +0530

    Create rectangle-area.py

---
+++ leetcode/rectangle-area.py
@@ -0,0 +1,5 @@
+# leetcode - https://leetcode.com/problems/rectangle-area/
+class Solution:
+    def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
+        intersect=max(min(C,G)-max(A,E),0)*max(min(D,H)-max(B,F),0)
+        return (A-C)*(B-D) + (E-G)*(F-H) - intersect
