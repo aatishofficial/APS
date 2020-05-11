470c25dec239ca51f9c192cc2587434d77f66153
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-05-11 19:41:01 +0530

    Create wiggle-sort-ii.py

---
+++ leetcode/wiggle-sort-ii.py
@@ -0,0 +1,9 @@
+# leetcode - https://leetcode.com/problems/wiggle-sort-ii/
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+        half=len(nums[::2])
+        nums[::2],nums[1::2]=nums[:half][::-1],nums[half:][::-1]
