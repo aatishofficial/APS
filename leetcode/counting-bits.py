2519b8a32d987ea1e5797f9a56b7280f39a35289
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-05-12 09:46:54 +0530

    Create counting-bits.py

---
+++ leetcode/counting-bits.py
@@ -0,0 +1,7 @@
+# leetcode - https://leetcode.com/problems/counting-bits/
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        res=[0]*(num+1)
+        for i in range(1,num+1):
+            res[i]=res[i & (i -1)] + 1
+        return res
