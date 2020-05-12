f81a4b4907fd36aeb184ff289859eb57eb051efe
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-05-12 10:11:07 +0530

    Create integer-break.py

---
+++ leetcode/integer-break.py
@@ -0,0 +1,13 @@
+# leetcode - https://leetcode.com/problems/integer-break
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n==2:
+            return 1
+        if n==3:
+            return 2
+        res=1
+        while n>4:
+            res*=3
+            n-=3
+        res*=n
+        return res
