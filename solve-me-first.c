19d6cbf55de1c2c568e00c347ed98755c9040f6f
aatishofficial <37585363+aatishofficial@users.noreply.github.com>
2020-05-22 17:30:43 +0530

    Create solve-me-first.c

---
+++ solve-me-first.c
@@ -0,0 +1,22 @@
+# hackerrank - https://www.hackerrank.com/challenges/solve-me-first/problem
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int solveMeFirst(int a, int b) {
+    // Hint: Type return a+b; below
+ int s;
+s=a+b;
+return s;
+}
+int main() {
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    int sum; 
+    sum = solveMeFirst(num1,num2);
+    printf("%d",sum);
+    return 0;
+}
